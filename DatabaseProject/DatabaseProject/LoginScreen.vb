17b75243567a810627531189df443c72d585b25c
as13770 <as13770@LIBR2081200X077>
2019-03-12 18:38:23 -0400

    Added Login Screen without backend

---
+++ DatabaseProject/DatabaseProject/LoginScreen.vb
@@ -0,0 +1,6 @@
+﻿Public Class LoginScreen
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+    End Sub
+End Class
