a1eea62ebc27df7341f2ceee459389ebee48d482
William Dew <wd01248@georgiasouthern.edu>
2019-04-01 14:49:58 -0400

    Gui for Trucks, Trains, and Ports

---
+++ DatabaseProject/DatabaseProject/Trucks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database_Project
+{
+    public partial class Trucks : Form
+    {
+        public Trucks()
+        {
+            InitializeComponent();
+        }
+
+        private void Trucks_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
